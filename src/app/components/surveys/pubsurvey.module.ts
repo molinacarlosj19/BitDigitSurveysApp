f1690ed321fe1adca331666f710918a1063def4a
dagutier-cntniall <dagutierrez@gmail.com>
2022-08-13 22:17:33 -0400

    Parallel structure to display Public Surveys (WIP)

--- src/app/components/surveys/pubsurvey.module.ts
+++ src/app/components/surveys/pubsurvey.module.ts
@@ -8,18 +8,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { SurveyListingComponent } from './survey-listing/survey-listing.component';
 import { SurveyEditComponent } from './survey-edit/survey-edit.component';
 import { SurveyCreateComponent } from './survey-create/survey-create.component';
+import { SurveyPublicListingComponent } from './survey-public-listing/survey-public-listing.component';
 import { SurveyResponseComponent } from './survey-response/survey-response.component';
 
 const routes = RouterModule.forChild([
-  { path: 'list', component: SurveyListingComponent, data: { title: 'Surveys' } },
-  { path: 'survey-edit/:id', component: SurveyEditComponent, data: { title: 'Surveys'} },
-  { path: 'survey-create', component: SurveyCreateComponent, data: { title: 'Surveys' } },
+  { path: '/list', component: SurveyPublicListingComponent, data: { title: 'Surveys' } },
+  { path: 'respond/:id', component: SurveyResponseComponent, data: { title: 'Surveys'} },
 ]);
 
 @NgModule({
   imports: [ModelModule, CommonModule, FormsModule, ReactiveFormsModule, routes],
   providers: [],
-  declarations: [SurveyListingComponent, SurveyEditComponent,SurveyCreateComponent,
-    ]
+  declarations: [SurveyEditComponent,SurveyCreateComponent,
+    SurveyPublicListingComponent]
 })
-export class SurveyModule {}
+export class PubSurveyModule {}
