5b4b83945784f71941a2c4d68701065b305a0bfb
dagutier-cntniall <102626521+dagutier-cntniall@users.noreply.github.com>
2022-08-18 17:47:12 -0400

    Update rest.datasource.ts

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -28,7 +28,7 @@ export class RestDataSource {
     };
 
   constructor(private http: HttpClient) {
-    this.baseUrl = `https://comp229-bitdigitsurveys-be.herokuapp.com//api/`;
+    this.baseUrl = `https://comp229-bitdigitsurveys-be.herokuapp.com/api/`;
   }
 
   getPubSurveys(): Observable<Surveys[]> {
